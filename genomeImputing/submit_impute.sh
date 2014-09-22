c407c7a87cbe33868d3cd9dc86e307970892b2fa
James Eddy <james.a.eddy@gmail.com>
2014-09-22 16:40:21 -0700

    created submit wrapper for impute script

---
+++ genomeImputing/submit_impute.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo ""
+
+# assign inputs
+GWAS_DATA=SYounkin_MayoGWAS_09-05-08
+
+# directories
+ROOT_DIR=./
+DATA_DIR=${ROOT_DIR}data/
+GWAS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.by_chr/
+
+for CHR in 22; do
+
+    NUM_FILE="${GWAS_DIR}impute_intervals/num_ints.txt"
+    NUM_INTS=$(awk -v chr=$CHR '$2==chr {print $3}' $NUM_FILE)
+    INTS_FILE="${GWAS_DIR}impute_intervals/chr${CHR}.ints"
+
+    for INT in 1; do
+
+        CHUNK_START=$(awk -v i=$INT '$3==i {print $5}' $INTS_FILE)
+        CHUNK_END=$(awk -v i=$INT '$3==i {print $6}' $INTS_FILE)
+                
+        ./impute.sh $GWAS_DATA $CHR $CHUNK_START $CHUNK_END
+    
+    done
+    
+done
