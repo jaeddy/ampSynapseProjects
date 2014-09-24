a0bc242313c056cf089c3221be8d93ef6531fe47
James Eddy <james.a.eddy@gmail.com>
2014-09-24 15:23:47 -0700

    updated & tested impute script

--- genomeImputing/impute.sh
+++ genomeImputing/impute.sh
@@ -11,8 +11,8 @@ ROOT_DIR=./
 DATA_DIR=${ROOT_DIR}data/
 GWAS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.b37/
 GENMAP_DIR=${DATA_DIR}haplotypes/
-# GWAS_HAPS_DIR=${GENMAP_DIR}${GWAS_DATA}.phased/
-GWAS_HAPS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.phased/
+REFHAPS_DIR=${GENMAP_DIR}1000genomes/
+GWAS_HAPS_DIR=${GENMAP_DIR}${GWAS_DATA}.phased/
 
 # prefixes and files
 HAP_PREFIX=".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing."
@@ -29,9 +29,9 @@ IMPUTE_EXEC=${ROOT_DIR}resources/impute2/impute2
 
 # specify data files
 GWAS_HAPS_FILE=${GWAS_HAPS_DIR}${GWAS_DATA}.chr${CHR}.phased.haps
-HAPS_FILE=${GENMAP_DIR}ALL.chr${CHR}${HAP_PREFIX}haplotypes.gz
-LEGEND_FILE=${GENMAP_DIR}ALL.chr${CHR}${HAP_PREFIX}legend.gz
-GENMAP_FILE=${GENMAP_DIR}genetic_map_chr${CHR}_combined_b37.txt
+HAPS_FILE=${REFHAPS_DIR}ALL.chr${CHR}${HAP_PREFIX}haplotypes.gz
+LEGEND_FILE=${REFHAPS_DIR}ALL.chr${CHR}${HAP_PREFIX}legend.gz
+GENMAP_FILE=${REFHAPS_DIR}genetic_map_chr${CHR}_combined_b37.txt
 
 RESULT_FILE="${GWAS_DATA}.chr${CHR}.pos${CHUNK_START}-${CHUNK_END}.imputed"
 
@@ -42,13 +42,14 @@ $IMPUTE_EXEC \
     -h $HAPS_FILE \
     -l $LEGEND_FILE \
     -known_haps_g $GWAS_HAPS_FILE \
-    -iter 30 \
-    -burnin 10 \
+    -iter 2 \
+    -burnin 1 \
     -k 80 \
     -k_hap 500 \
     -Ne 20000 \
     -int $CHUNK_START $CHUNK_END \
     -allow_large_regions \
-    -o ./resources/impute2/Example/example.chr22.one.phased.impute2
+    -filt_rules_l 'eur.maf==0' \
+    -o $RESULT_FILE ;
 
-# mv *.chr${CHR}.*.imputed* "$RESULTS_DIR"
+mv *.chr${CHR}.*.imputed* "$RESULTS_DIR"
