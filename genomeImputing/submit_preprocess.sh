a1e97c7fa866d4a0c5f0409e455d85e63223356f
John C. Earls <john.c.earls@gmail.com>
2014-09-25 23:08:42 +0000

    modified to run for all chromosomes

--- genomeImputing/submit_preprocess.sh
+++ genomeImputing/submit_preprocess.sh
@@ -5,8 +5,8 @@ echo ""
 # assign inputs
 GWAS_DATA=SYounkin_MayoGWAS_09-05-08
 
-for CHR in 22; do
+for CHR in $(seq 1 22); do
                 
-    qsub -V ./preprocess.sh $GWAS_DATA $CHR
+    qsub -V -cwd ./preprocess.sh $GWAS_DATA $CHR
         
 done
