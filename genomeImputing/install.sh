8135f20e622a3a1963a50d33c2b9ebdb4b7f889c
James Eddy <james.a.eddy@gmail.com>
2014-09-25 14:53:24 -0700

    created script to install binaries remotely

---
+++ genomeImputing/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# directories
+ROOT_DIR=./
+
+# create new directory to store resources
+RESOURCES_DIR=${ROOT_DIR}resources/
+
+if [ ! -e "$RESOURCES_DIR" ]; then
+    mkdir "$RESOURCES_DIR"
+fi
+
+cd $RESOURCES_DIR
+
+# install PLINK
+wget http://pngu.mgh.harvard.edu/~purcell/plink/dist/plink-1.07-x86_64.zip
+unzip plink-1.07-x86_64.zip
+mv plink-1.07-x86_64 plink
+
+# install liftOver
+wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/liftOver
+
+# install IMPUTE2
+wget https://mathgen.stats.ox.ac.uk/impute/impute_v2.3.1_x86_64_static.tgz
+tar -zxvf impute_v2.3.1_x86_64_static.tgz
+mv impute_v2.3.1_x86_64_static impute2
+
+# install SHAPEIT
+wget https://mathgen.stats.ox.ac.uk/genetics_software/shapeit/shapeit.v2.r790.Ubuntu_12.04.4.static.tar.gz
+tar -zxvf shapeit.v2.r790.Ubuntu_12.04.4.static.tar.gz
+mv shapeit.v2.r790.Ubuntu_12.04.4.static shapeit
+
+# install qctool
+wget http://www.well.ox.ac.uk/~gav/qctool/resources/qctool_v1.4-linux-x86_64.tgz
+tar -zxvf qctool_v1.4-linux-x86_64.tgz
+mv qctool_v1.4-linux-x86_64 qctool
+
+cd $ROOT_DIR
