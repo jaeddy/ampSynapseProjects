280db61157112aa355af28b3392327e22c347423
James Eddy <james.a.eddy@gmail.com>
2014-09-26 12:51:46 -0700

    added script to copy data from S3

---
+++ genomeImputing/get_data.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# directories
+ROOT_DIR=./
+
+# create new directory to store data
+DATA_DIR=${ROOT_DIR}data/
+
+if [ ! -e "$DATA_DIR" ]; then
+    mkdir "$DATA_DIR"
+fi
+
+# copy data from S3 bucket
+aws s3 cp s3://mayo-gwas-impute $DATA_DIR --recursive
