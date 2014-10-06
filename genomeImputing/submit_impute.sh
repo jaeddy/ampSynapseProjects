8beb0e2288fe65a895800ff13ccf666eb903fdfa
root <james.a.eddy@gmail.com>
2014-10-06 16:59:20 +0000

    updated submit_impute to run for all intervals

--- genomeImputing/submit_impute.sh
+++ genomeImputing/submit_impute.sh
@@ -16,7 +16,7 @@ for CHR in $(seq 1 22); do
     NUM_INTS=$(awk -v chr=$CHR '$2==chr {print $3}' $NUM_FILE)
     INTS_FILE="${GWAS_DIR}impute_intervals/chr${CHR}.ints"
 
-    for INT in 1; do
+    for INT in $(seq 1 $NUM_INTS); do
 
         CHUNK_START=$(awk -v i=$INT '$3==i {print $5}' $INTS_FILE)
         CHUNK_END=$(awk -v i=$INT '$3==i {print $6}' $INTS_FILE)
