9997117181f6ed2c502a1a3c316a6a73f82243f0
James Eddy <james.a.eddy@gmail.com>
2014-10-28 12:43:25 -0700

    code to extract/format clinical covariate data

---
+++ synapseUploads/R/extract_gwas_covars.R
@@ -0,0 +1,19 @@
+library(xlsx)
+
+setwd("synapseUploads/")
+
+mayo_covars <- read.xlsx2("data/TLR5+GWAScov_10-28-14_0912_CM+SGY.xlsx", 1,
+                          stringsAsFactors = FALSE, na.strings = "NA")
+load_gwas_fam <- read.table("data/SYounkin_MayoGWAS_09-05-08.fam")
+names(load_gwas_fam) <- c("family_id", "sample_id", "paternal_id",
+                          "maternal_id", "sex", "phenotype")
+
+names(mayo_covars)
+extract_cols <- names(mayo_covars)[4:13]
+
+no_id <- mayo_covars$IlluminaIID...IID %in% "NA"
+gwas_covars <- mayo_covars[!no_id, extract_cols]
+
+load_gwas_covars <- merge(x = load_gwas_fam, y = gwas_covars,
+                          by.x = "sample_id", by.y = "IlluminaIID...IID",
+                          all = TRUE)
