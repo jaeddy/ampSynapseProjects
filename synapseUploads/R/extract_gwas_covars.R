1bb271b363ab8e90d20df34cfd34cc23df52340e
James Eddy <james.a.eddy@gmail.com>
2014-12-02 14:16:08 -0800

    fixing local paths

--- synapseUploads/R/extract_gwas_covars.R
+++ synapseUploads/R/extract_gwas_covars.R
@@ -1,11 +1,14 @@
 library(xlsx)
 
 setwd("synapseUploads/")
-dataDir <- "~/Dropbox/data/projects/"
+dataDir <- "~/Dropbox/data/projects/ampSynapseProjects/synapseUploads"
 
+mayo_covars_path <- file.path(dataDir, "TLR5+GWAScov_10-28-14_0912_CM+SGY.xlsx")
 mayo_covars <- read.xlsx2("data/TLR5+GWAScov_10-28-14_0912_CM+SGY.xlsx", 1,
                           stringsAsFactors = FALSE, na.strings = "NA")
-load_gwas_fam <- read.table("data/SYounkin_MayoGWAS_09-05-08.fam")
+
+gwas_fam_path <- file.path(dataDir, "SYounkin_MayoGWAS_09-05-08.fam")
+load_gwas_fam <- read.table(gwas_fam_path)
 names(load_gwas_fam) <- c("family_id", "sample_id", "paternal_id",
                           "maternal_id", "sex", "phenotype")
 
@@ -19,4 +22,5 @@ load_gwas_covars <- merge(x = load_gwas_fam, y = gwas_covars,
                           by.x = "sample_id", by.y = "IlluminaIID...IID",
                           all = TRUE)
 
-write.xlsx(load_gwas_covars, "data/LOAD_GWAS_covars.xlsx")
+output_path <- file.path(dataDir, "LOAD_GWAS_covars.xlsx")
+write.xlsx(load_gwas_covars, output_path)
