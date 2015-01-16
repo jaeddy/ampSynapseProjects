865a5465ba6fd7f506d13092f03819e84bdc2d74
James Eddy <james.a.eddy@gmail.com>
2015-01-15 17:00:12 -0800

    skeleton script to set AMP project annotations

---
+++ synapseUploads/R/set_annotations.R
@@ -0,0 +1,7 @@
+
+entity <- ""
+fields <- c("dataset_name", "data_type", "feature_type", "object_id", 
+            "file_format", "num_files", "species", "strain", "num_samples", 
+            "num_features", "phenotypes", "file_status", "data_status",
+            "source_file", "readme", "activity", "source", "study",
+            "date_uploaded", "public", "protected")
