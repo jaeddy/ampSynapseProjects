23af1c49a32cfbba1f443459cd18410c85829c5b
James Eddy <james.a.eddy@gmail.com>
2015-01-29 18:39:54 -0500

    setting up script to run with s3 data

--- genomeImputing/merge_qc.sh
+++ genomeImputing/merge_qc.sh
@@ -7,12 +7,19 @@ GWAS_DATA=SYounkin_MayoGWAS_09-05-08
 CHR=22
 
 # directories
+S3_BUCKET=s3://mayo-gwas-impute
 ROOT_DIR=./
-DATA_DIR=${ROOT_DIR}data/
-GWAS_DIR=${DATA_DIR}gwas_results/
-GENMAP_DIR=${DATA_DIR}haplotypes/
-GWAS_HAP_DIR=${GENMAP_DIR}${GWAS_DATA}.phased/
+DATA_DIR=/mnt/${ROOT_DIR}data/
+
+# if [ ! -e "$DATA_DIR" ]; then
+# 	mkdir "$DATA_DIR"
+# fi
+
+
+GWAS_DIR=gwas_results/
+GENMAP_DIR=haplotypes/
 GWAS_IMP_DIR=${GWAS_DIR}${GWAS_DATA}.imputed/
+GWAS_HAP_DIR=${GENMAP_DIR}${GWAS_DATA}.phased/
 
 # executables
 GTOOL_EXEC=${ROOT_DIR}resources/gtool
@@ -21,38 +28,49 @@ QCTOOL_EXEC=${ROOT_DIR}resources/qctool/qctool
 # specify additional data files
 SAMPLE_FILE=${GWAS_HAP_DIR}${GWAS_DATA}.chr${CHR}.phased.sample
 
+# copy data from S3 bucket
+aws s3 cp --dryrun \ # .sample file
+	${S3_BUCKET}${SAMPLE_FILE} \
+	${DATA_DIR}${SAMPLE_FILE}
+
+aws s3 cp --dryrun \ # imputed genotype files
+	${S3_BUCKET}${GWAS_IMP_DIR} \
+	${DATA_DIR}${GWAS_IMP_DIR} \
+	--recursive --exclude "*" --include "*chr${CHR}*"
+
 # get list of imputed genotype files for chromosome
-CHUNK_LIST=$(ls -d -1 ${GWAS_IMP_DIR}*.* | grep "chr${CHR}.*.imputed$")
+CHUNK_LIST=$(ls -d -1 ${DATA_DIR}${GWAS_IMP_DIR}*.* \
+	| grep "chr${CHR}.*.imputed$")
 
 # merge all imputed genotype files for chromosome
-GEN_FILE="${GWAS_IMP_DIR}${GWAS_DATA}.chr${CHR}.imputed.gen"
+GEN_FILE="${DATA_DIR}${GWAS_IMP_DIR}${GWAS_DATA}.chr${CHR}.imputed.gen"
 
 echo "Merging all chunk files for chromsome ${CHR}..."
 echo
 cat $CHUNK_LIST > $GEN_FILE
 
 # create new directory to store results
-RESULTS_DIR=${GWAS_DIR}${GWAS_DATA}.imputed.qc/
+RESULTS_DIR=${DATA_DIR}${GWAS_DIR}${GWAS_DATA}.imputed.qc/
 
-if [ ! -e "$RESULTS_DIR" ]; then
-	mkdir "$RESULTS_DIR"
-fi
+# if [ ! -e "$RESULTS_DIR" ]; then
+# 	mkdir "$RESULTS_DIR"
+# fi
 
 # perform QC with qctool
 QC_FILE=${RESULTS_DIR}${GWAS_DATA}.chr${CHR}.imputed.qc.gen
 
-echo "Performing QC on merged file..."
+echo "Performing QC on merged file ${GEN_FILE}..."
 echo
-time $QCTOOL_EXEC -g $GEN_FILE -og $QC_FILE \
-	-snp-missing-rate 0.05 -maf 0 1 -info 0.4 1 -hwe 20
+# time $QCTOOL_EXEC -g $GEN_FILE -og $QC_FILE \
+# 	-snp-missing-rate 0.05 -maf 0 1 -info 0.4 1 -hwe 20
 
 # qctool adds an extra columns of NA for some reason; need to remove
 TMP_FILE=`mktemp qcgen.XXX`
 
-echo "Removing extraneous first coumn from QC output..."
+echo "Removing extraneous first coumn from QC output ${QC_FILE}..."
 echo
-cut -d " " -f 2- $QC_FILE > $TMP_FILE
-mv $TMP_FILE $QC_FILE
+# cut -d " " -f 2- $QC_FILE > $TMP_FILE
+# mv $TMP_FILE $QC_FILE
 
 # convert to ped/map with gtool
 PED_FILE="${RESULTS_DIR}${GWAS_DATA}.chr${CHR}.imputed.ped"
@@ -63,14 +81,14 @@ echo
 
 echo "Converting gen/sample format to ped/map..."
 echo
-time $GTOOL_EXEC -G --g $QC_FILE --s $SAMPLE_FILE \
- 	--ped $PED_FILE --map $MAP_FILE \
- 	--phenotype plink_pheno --chr ${CHR}
+# time $GTOOL_EXEC -G --g $QC_FILE --s $SAMPLE_FILE \
+#  	--ped $PED_FILE --map $MAP_FILE \
+#  	--phenotype plink_pheno --chr ${CHR}
 
 # gtool swaps the first 2 columns of the ped file; switch back
-TMP_FILE=`mktemp ped.XXX`
+# TMP_FILE=`mktemp ped.XXX`
 
 echo "Swaping first two columns of gtool generated ped file..."
 echo
-paste <(cut -f 1-2 $PED_FILE | awk '{OFS = "\t"; t = $1; $1 = $2; $2 = t; print}') <(cut -f 3- $PED_FILE) > $TMP_FILE
-mv $TMP_FILE $PED_FILE
+# paste <(cut -f 1-2 $PED_FILE | awk '{OFS = "\t"; t = $1; $1 = $2; $2 = t; print}') <(cut -f 3- $PED_FILE) > $TMP_FILE
+# mv $TMP_FILE $PED_FILE
