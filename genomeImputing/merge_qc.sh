2e26805be360228fa3ff245ffd124a7c969a1828
James Eddy <james.a.eddy@gmail.com>
2015-01-30 02:06:12 -0500

    updated submit script, ready to run

--- genomeImputing/merge_qc.sh
+++ genomeImputing/merge_qc.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # assign inputs
-# GWAS_DATA="$1"
-# CHR="$2"
-GWAS_DATA=SYounkin_MayoGWAS_09-05-08
-CHR=22
+GWAS_DATA="$1"
+CHR="$2"
 
 # directories
 S3_BUCKET=s3://mayo-gwas-impute/
@@ -32,25 +30,25 @@ SAMPLE_FILE=${GWAS_HAP_DIR}${GWAS_DATA}.chr${CHR}.phased.sample
 
 echo "Downloading data from S3..."
 
-#aws s3 cp \
-#    ${S3_BUCKET}${SAMPLE_FILE} \
-#    ${DATA_DIR}${SAMPLE_FILE}
+aws s3 cp \
+   ${S3_BUCKET}${SAMPLE_FILE} \
+   ${DATA_DIR}${SAMPLE_FILE}
 
-#aws s3 cp \
-#    ${S3_BUCKET}${GWAS_IMP_DIR} \
-#    ${DATA_DIR}${GWAS_IMP_DIR} \
-#    --recursive --exclude "*" --include "*chr${CHR}*.imputed"
+aws s3 cp \
+   ${S3_BUCKET}${GWAS_IMP_DIR} \
+   ${DATA_DIR}${GWAS_IMP_DIR} \
+   --recursive --exclude "*" --include "*chr${CHR}*.imputed"
 
 # get list of imputed genotype files for chromosome
-#CHUNK_LIST=$(ls -d -1 ${DATA_DIR}${GWAS_IMP_DIR}*.* \
-#    | grep "chr${CHR}.*.imputed$")
+CHUNK_LIST=$(ls -d -1 ${DATA_DIR}${GWAS_IMP_DIR}*.* \
+   | grep "chr${CHR}.*.imputed$")
 
 # merge all imputed genotype files for chromosome
 GEN_FILE="${DATA_DIR}${GWAS_IMP_DIR}${GWAS_DATA}.chr${CHR}.imputed.gen"
 
 echo "Merging all chunk files for chromsome ${CHR}..."
 echo
-#cat $CHUNK_LIST > $GEN_FILE
+cat $CHUNK_LIST > $GEN_FILE
 
 # create new directory to store results
 RESULTS_DIR=${GWAS_DIR}${GWAS_DATA}.imputed.qc/
@@ -64,16 +62,16 @@ QC_FILE=${RESULTS_DIR}${GWAS_DATA}.chr${CHR}.imputed.qc.gen
 
 echo "Performing QC on merged file ${GEN_FILE}..."
 echo
-#time $QCTOOL_EXEC -g $GEN_FILE -og ${DATA_DIR}${QC_FILE} \
-#    -snp-missing-rate 0.05 -maf 0 1 -info 0.4 1 -hwe 20
+time $QCTOOL_EXEC -g $GEN_FILE -og ${DATA_DIR}${QC_FILE} \
+   -snp-missing-rate 0.05 -maf 0 1 -info 0.4 1 -hwe 20
 
 # qctool adds an extra columns of NA for some reason; need to remove
-#TMP_FILE=`mktemp qcgen.XXX`
+TMP_FILE=`mktemp qcgen.XXX`
 
 echo "Removing extraneous first coumn from QC output ${QC_FILE}..."
 echo
-#cut -d " " -f 2- ${DATA_DIR}${QC_FILE} > $TMP_FILE
-#mv $TMP_FILE ${DATA_DIR}${QC_FILE}
+cut -d " " -f 2- ${DATA_DIR}${QC_FILE} > $TMP_FILE
+mv $TMP_FILE ${DATA_DIR}${QC_FILE}
 
 # convert to ped/map with gtool
 PED_FILE="${RESULTS_DIR}${GWAS_DATA}.chr${CHR}.imputed.ped"
@@ -84,25 +82,26 @@ echo
 
 echo "Converting gen/sample format to ped/map..."
 echo
-#time $GTOOL_EXEC -G \
-#    --g ${DATA_DIR}${QC_FILE} --s ${DATA_DIR}${SAMPLE_FILE} \
-#    --ped ${DATA_DIR}${PED_FILE} --map ${DATA_DIR}${MAP_FILE} \
-#    --phenotype plink_pheno --chr ${CHR}
+time $GTOOL_EXEC -G \
+   --g ${DATA_DIR}${QC_FILE} --s ${DATA_DIR}${SAMPLE_FILE} \
+   --ped ${DATA_DIR}${PED_FILE} --map ${DATA_DIR}${MAP_FILE} \
+   --phenotype plink_pheno --chr ${CHR}
 
 #gtool swaps the first 2 columns of the ped file; switch back
-#TMP_FILE=`mktemp ped.XXX`
+TMP_FILE=`mktemp ped.XXX`
 
 echo "Swapping first two columns of gtool generated ped file..."
 echo
-#paste <(cut -f 1-2 ${DATA_DIR}${PED_FILE} | awk '{OFS = "\t"; t = $1; $1 = $2; $2 = t; print}') \
-#    <(cut -f 3- ${DATA_DIR}${PED_FILE}) > $TMP_FILE
-#mv $TMP_FILE ${DATA_DIR}${PED_FILE}
+paste <(cut -f 1-2 ${DATA_DIR}${PED_FILE} \
+    | awk '{OFS = "\t"; t = $1; $1 = $2; $2 = t; print}') \
+    <(cut -f 3- ${DATA_DIR}${PED_FILE}) > $TMP_FILE
+mv $TMP_FILE ${DATA_DIR}${PED_FILE}
 
 # copy qc'd files to S3
 
 echo "Uploading results to S3..."
 echo
-aws s3 cp --dryrun \
+aws s3 cp \
     ${DATA_DIR}${RESULTS_DIR} \
     ${S3_BUCKET}${RESULTS_DIR} \
     --recursive --exclude "*" --include "*chr${CHR}*"
--- genomeImputing/submit_qc.sh
+++ genomeImputing/submit_qc.sh
@@ -5,25 +5,9 @@ echo ""
 # assign inputs
 GWAS_DATA=SYounkin_MayoGWAS_09-05-08
 
-# directories
-ROOT_DIR=./
-DATA_DIR=${ROOT_DIR}data/
-GWAS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.b37/
-
 for CHR in $(seq 1 22); do
 
-    NUM_FILE="${GWAS_DIR}impute_intervals/num_ints.txt"
-    NUM_INTS=$(awk -v chr=$CHR '$2==chr {print $3}' $NUM_FILE)
-    INTS_FILE="${GWAS_DIR}impute_intervals/chr${CHR}.ints"
-
-    for INT in 1; do
-
-        CHUNK_START=$(awk -v i=$INT '$3==i {print $5}' $INTS_FILE)
-        CHUNK_END=$(awk -v i=$INT '$3==i {print $6}' $INTS_FILE)
-                
-        qsub -V -cwd ./postqc.sh $GWAS_DATA $CHR $CHUNK_START $CHUNK_END \
-            -M james.a.eddy@gmail.com ;
-    done
+    qsub -V -cwd ./merge_qc.sh $GWAS_DATA $CHR \
+    -M james.a.eddy@gmail.com ;
 
 done
-    
