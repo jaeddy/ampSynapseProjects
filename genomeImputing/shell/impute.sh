3a545eead04a1716392e8f5fac71c94934c5563f
James Eddy <james.a.eddy@gmail.com>
2015-02-02 16:02:39 -0800

    modified impute script to work with s3

--- genomeImputing/shell/impute.sh
+++ genomeImputing/shell/impute.sh
@@ -7,49 +7,83 @@ CHUNK_START=`printf "%.0f" $3`
 CHUNK_END=`printf "%.0f" $4`
 
 # directories
+S3_BUCKET=s3://mayo-gwas-impute/
 ROOT_DIR=./
-DATA_DIR=${ROOT_DIR}data/
-GWAS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.b37/
-GENMAP_DIR=${DATA_DIR}haplotypes/
-REFHAPS_DIR=${GENMAP_DIR}1000genomes/
-GWAS_HAPS_DIR=${GENMAP_DIR}${GWAS_DATA}.phased/
+DATA_DIR=/mnt/data/
 
-# prefixes and files
-HAP_PREFIX=".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing."
-
-# create new directory to store results
-RESULTS_DIR=${DATA_DIR}gwas_results/${GWAS_DATA}.imputed/
-
-if [ ! -e "$RESULTS_DIR" ]; then
-    mkdir "$RESULTS_DIR"
+if [ ! -e "$DATA_DIR" ]; then
+    mkdir "$DATA_DIR"
 fi
 
-# executable
+GWAS_DIR=gwas_results/
+HAPS_DIR=haplotypes/
+REFHAPS_DIR=${HAPS_DIR}1000genomes/
+GWAS_HAPS_DIR=${HAPS_DIR}${GWAS_DATA}.phased/
+
+# reference prefix
+HAP_PREFIX=".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing."
+
+# executables
 IMPUTE_EXEC=${ROOT_DIR}resources/impute2/impute2
+
+# create new directory to store results
+RESULTS_DIR=${GWAS_DIR}${GWAS_DATA}.imputed/
+
+if [ ! -e "${DATA_DIR}${RESULTS_DIR}" ]; then
+    mkdir "${DATA_DIR}${RESULTS_DIR}"
+fi
 
 # specify data files
 GWAS_HAPS_FILE=${GWAS_HAPS_DIR}${GWAS_DATA}.chr${CHR}.phased.haps
 HAPS_FILE=${REFHAPS_DIR}ALL.chr${CHR}${HAP_PREFIX}haplotypes.gz
 LEGEND_FILE=${REFHAPS_DIR}ALL.chr${CHR}${HAP_PREFIX}legend.gz
-GENMAP_FILE=${REFHAPS_DIR}genetic_map_chr${CHR}_combined_b37.txt
+MAP_FILE=${REFHAPS_DIR}genetic_map_chr${CHR}_combined_b37.txt
+
+# copy data from S3 bucket
+echo "Downloading data from S3..."
+# get pre-phased data
+aws s3 cp --dryrun \
+    ${S3_BUCKET}${GWAS_HAPS_FILE} \
+    ${DATA_DIR}${GWAS_HAPS_FILE}
+
+# get reference data
+aws s3 cp --dryrun \
+    ${S3_BUCKET}${HAPS_FILE}
+    ${DATA_DIR}${HAPS_FILE}
+
+aws s3 cp --dryrun \
+    ${S3_BUCKET}${LEGEND_FILE}
+    ${DATA_DIR}${LEGEND_FILE}
+
+aws s3 cp --dryrun \
+    ${S3_BUCKET}${MAP_FILE}
+    ${DATA_DIR}${MAP_FILE}
 
 RESULT_FILE="${GWAS_DATA}.chr${CHR}.pos${CHUNK_START}-${CHUNK_END}.imputed"
 
 # impute estimated haplotype files with impute2 and save to results directory
-$IMPUTE_EXEC \
-    -use_prephased_g \
-    -m $GENMAP_FILE \
-    -h $HAPS_FILE \
-    -l $LEGEND_FILE \
-    -known_haps_g $GWAS_HAPS_FILE \
-    -iter 30 \
-    -burnin 10 \
-    -k 80 \
-    -k_hap 500 \
-    -Ne 20000 \
-    -int $CHUNK_START $CHUNK_END \
-    -allow_large_regions \
-    -filt_rules_l 'eur.maf==0' \
-    -o $RESULT_FILE ;
+echo "Reprocessing genotype data for chromosome ${CHR}..."
+echo
+# $IMPUTE_EXEC \
+#     -use_prephased_g \
+#     -m ${DATA_DIR}${MAP_FILE} \
+#     -h ${DATA_DIR}${HAPS_FILE} \
+#     -l ${DATA_DIR}${LEGEND_FILE} \
+#     -known_haps_g ${DATA_DIR}${GWAS_HAPS_FILE} \
+#     -iter 30 \
+#     -burnin 10 \
+#     -k 80 \
+#     -k_hap 500 \
+#     -Ne 20000 \
+#     -int $CHUNK_START $CHUNK_END \
+#     -allow_large_regions \
+#     -filt_rules_l 'eur.maf==0' \
+#     -o ${DATA_DIR}${RESULT_FILE} ;
 
-mv *.chr${CHR}.*.imputed* "$RESULTS_DIR"
+# copy chr-specific files to S3
+echo "Uploading results to S3..."
+echo
+# aws s3 cp \
+#     ${DATA_DIR}${RESULTS_DIR} \
+#     ${S3_BUCKET}${RESULTS_DIR} \
+#     --recursive --exclude "*" --include "*chr${CHR}.*"
