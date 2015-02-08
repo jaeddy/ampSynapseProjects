b18d22c8fc38e4c9865173f65a96d2ed34380975
James Eddy <james.a.eddy@gmail.com>
2015-02-07 18:02:10 -0800

    updated script for formatting tcx rnaseq sample info

--- dataEnablement/R/format_tcx_rnaseq_covars_groups.R
+++ dataEnablement/R/format_tcx_rnaseq_covars_groups.R
@@ -11,9 +11,10 @@ library(dplyr)
 synapseLogin()
 
 # Define paths for required Synapse objects
-tcx_rnaseq_id <- "syn3157267" # pilot RNAseq covariates Excel file
-clinical_folder_id <- "syn2866158" # id of destination folder
-parent_folder_id <- "syn3163039" # id of parent folder
+tcx_rnaseq_id <- "syn3163262" # pilot RNAseq covariates Excel file
+clinical_folder_id <- "syn2866158" # id of clinical covars folder
+technical_folder_id <- "syn3163735" # id of technical covars folder
+rnaseq_folder_id <- "syn3163039" # id of RNAseq folder
 
 # Download files from Synapse
 tcx_rnaseq_file <- synGet(tcx_rnaseq_id)
@@ -29,18 +30,22 @@ headers <- c("participant_id", "age_at_onset", "age_at_last_assessment",
 ##################
 
 # Read AD info table to data frame
-ad_rnaseq_info <- read.xlsx2(pilot_rnaseq_path, sheetIndex = 1,
+tcx_rnaseq_info <- read.xlsx2(tcx_rnaseq_path, sheetIndex = 1,
                              stringsAsFactors = FALSE)
 
 # Get clinical variables
-ad_rnaseq_clinical <- ad_rnaseq_info %>%
+tcx_rnaseq_clinical <- tcx_rnaseq_info %>%
+    
+    # select only Mayo samples
+    filter(Source != "RUSH-BROAD") %>%
+    
     # pull out relevant variables from original data frame
-    select(participant_id = Path_ID, age_at_diagnosis = Age, 
-           sex = Sex) %>%
+    select(participant_id = RNASubjectId, age_at_diagnosis = Age, 
+           sex = Sex, braak_stage = Braak) %>%
     
     # rename and modify variables to match template
-    mutate(age_at_last_assessment = as.numeric(age_at_diagnosis),
-           sex = ifelse(sex == 0, "M", "F")) %>%
+    mutate(age = as.numeric(age_at_diagnosis),
+           age_at_last_assessment = ifelse(age > 90, "censored", age)) %>%
     
     # add empty columns for additional template variables
     mutate(age_at_onset = NA,
@@ -49,7 +54,6 @@ ad_rnaseq_clinical <- ad_rnaseq_info %>%
            education = NA,
            apoe_genotype = NA,
            race_ethnicity = NA,
-           braak_stage = NA,
            mmse_at_onset = NA,
            mmse_at_last_assessment = NA,
            cerad = NA) %>%
@@ -58,85 +62,54 @@ ad_rnaseq_clinical <- ad_rnaseq_info %>%
     select(one_of(headers))
 
 # Save to file
-out_path <- file.path(tempdir(), "ad_pilot_rnaseq_clinical_vars.txt")
-write.table(ad_rnaseq_clinical, out_path, quote = FALSE, row.names = FALSE)
+out_path <- file.path(tempdir(), "mayo_tcx_rnaseq_clinical_vars.txt")
+write.table(tcx_rnaseq_clinical, out_path, quote = FALSE, row.names = FALSE)
 
 # Create a Synapse object for the output file and upload
-ad_rnaseq_clinical_object <- File(path = out_path, 
+tcx_rnaseq_clinical_object <- File(path = out_path, 
                                   parentId = clinical_folder_id)
-ad_rnaseq_clinical_object <- synStore(ad_rnaseq_clinical_object)
+tcx_rnaseq_clinical_object <- synStore(tcx_rnaseq_clinical_object)
+
 
 # Get technical variables
-ad_rnaseq_technical <- ad_rnaseq_info %>%
+tcx_rnaseq_technical <- tcx_rnaseq_info %>%
+    
+    # select only Mayo samples
+    filter(Source != "RUSH-BROAD") %>%
+    
     # pull out relevant variables from original data frame
-    select(Path_ID, IlluminaSampleID, Sequence.ID, RIN, RINsqAdj, 
-           Library.Batch, FCC1MR9ACXX, FCD1K78ACXX, FCD1LTPACXX, FCD1LUUACXX,
-           Flowcell)
+    select(RNASubjectId, RNAId, Source, Tissue, RIN)
 
 # Save to file
-out_path <- file.path(tempdir(), "ad_pilot_rnaseq_tech_vars.txt")
-write.table(ad_rnaseq_technical, out_path, quote = FALSE, row.names = FALSE)
+out_path <- file.path(tempdir(), "mayo_tcx_rnaseq_tech_vars.txt")
+write.table(tcx_rnaseq_technical, out_path, quote = FALSE, row.names = FALSE)
 
 # Create a Synapse object for the output file and upload
-ad_rnaseq_technical_object <- File(path = out_path, 
+tcx_rnaseq_technical_object <- File(path = out_path, 
                                    parentId = technical_folder_id)
-ad_rnaseq_technical_object <- synStore(ad_rnaseq_technical_object)
+tcx_rnaseq_technical_object <- synStore(tcx_rnaseq_technical_object)
 
 
-### PSP samples ###
-###################
-
-# Read PSP info table to data frame
-psp_rnaseq_info <- read.xlsx2(pilot_rnaseq_path, sheetIndex = 2,
-                              stringsAsFactors = FALSE)
-
-# Get clinical variables
-psp_rnaseq_clinical <- psp_rnaseq_info %>%
-    # pull out relevant variables from original data frame
-    select(participant_id = Path_ID, age_at_diagnosis = Age, 
-           sex = Sex) %>%
+# Format sample groups
+tcx_rnaseq_sample_groups <- tcx_rnaseq_info %>%
     
-    # rename and modify variables to match template
-    mutate(age_at_last_assessment = as.numeric(age_at_diagnosis),
-           sex = ifelse(sex == 0, "M", "F")) %>%
+    # select only Mayo samples
+    filter(Source != "RUSH-BROAD") %>%
     
-    # add empty columns for additional template variables
-    mutate(age_at_onset = NA,
-           age_at_death = NA,
-           post_mortem_interval = NA,
-           education = NA,
-           apoe_genotype = NA,
-           race_ethnicity = NA,
-           braak_stage = NA,
-           mmse_at_onset = NA,
-           mmse_at_last_assessment = NA,
-           cerad = NA) %>%
+    # pull out sample IDs and diagnosis
+    select(RNAId, FinalDx) %>%
     
-    # reorder variables to match template column order
-    select(one_of(headers))
+    # reformat diagnoses
+    mutate(is_AD = ifelse(FinalDx == "AD", 1, 0),
+           is_PSP = ifelse(FinalDx == "PSP", 1, 0),
+           is_PathAging = ifelse(FinalDx == "Pathological Aging", 1, 0),
+           is_Control = ifelse(FinalDx == "Control", 1, 0))
 
 # Save to file
-out_path <- file.path(tempdir(), "psp_pilot_rnaseq_clinical_vars.txt")
-write.table(psp_rnaseq_clinical, out_path, quote = FALSE, row.names = FALSE)
+out_path <- file.path(tempdir(), "mayo_tcx_rnaseq_sample_groups.txt")
+write.table(tcx_rnaseq_sample_groups, out_path, quote = FALSE, row.names = FALSE)
 
 # Create a Synapse object for the output file and upload
-psp_rnaseq_clinical_object <- File(path = out_path, 
-                                   parentId = clinical_folder_id)
-psp_rnaseq_clinical_object <- synStore(psp_rnaseq_clinical_object)
-
-
-# Get technical variables
-psp_rnaseq_technical <- psp_rnaseq_info %>%
-    # pull out relevant variables from original data frame
-    select(Path_ID, IlluminaSampleID, Sequence.ID, RIN, RINsqAdj, 
-           Library.Batch, FCD1GH3ACXX, FCD1KHGACXX, FCC1CDJACXX, Flowcell)
-
-# Save to file
-out_path <- file.path(tempdir(), "psp_pilot_rnaseq_tech_vars.txt")
-write.table(psp_rnaseq_technical, out_path, quote = FALSE, row.names = FALSE)
-
-# Create a Synapse object for the output file and upload
-psp_rnaseq_technical_object <- File(path = out_path, 
-                                    parentId = technical_folder_id)
-psp_rnaseq_technical_object <- synStore(psp_rnaseq_technical_object)
-
+tcx_rnaseq_sample_groups_object <- File(path = out_path, 
+                                    parentId = rnaseq_folder_id)
+tcx_rnaseq_sample_groups_object <- synStore(tcx_rnaseq_sample_groups_object)
